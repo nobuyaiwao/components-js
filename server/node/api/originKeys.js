7d951357f0afb0100ec5eeab89561342b67441c9
nobuyaiwao <nobuyaiwao@gmail.com>
2020-02-27 11:24:07 +0900

    originkeys

--- server/node/api/originKeys.js
+++ server/node/api/originKeys.js
@@ -4,8 +4,10 @@ const getPostParameters = require('../utils/getPostParameters');
 const handleCallback = require('../utils/handleCallback');
 
 module.exports = (res, request) => {
-    const originDomains = [`${request.protocol}://${request.headers.host}`];
+    const originDomains = [`${request.protocol}://${request.hostname}`];
+    //const originDomains = [`${request.protocol}://${request.headers.host}`];
     const params = getPostParameters('originKeys', { originDomains });
+    console.log(originDomains);
 
     post(params, (err, response, body) => handleCallback({ err, response, body }, res));
 };
